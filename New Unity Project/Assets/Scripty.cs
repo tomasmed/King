495e057e3f521bd024bdd4b639d2cea42099753b
hkawoosa <hkawoosa@gmail.com>
2016-10-15 14:22:42 -0400

    timer

--- New Unity Project/Assets/Scripty.cs
+++ New Unity Project/Assets/Scripty.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-
+/*
 List<Transform> tList = List<Transform>();
 
 
 createpoints();
 
 addpointstolist();
-
+*/
 
 public class Scripty : MonoBehaviour {
 
