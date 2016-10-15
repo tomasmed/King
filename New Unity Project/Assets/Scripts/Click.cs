6fdfe120d252817e893f9d2509a704f68aeff79d
dannybee722 <dbraunst@umich.edu>
2016-10-15 18:01:33 -0400

    edited click.cs to have 3 conditions for placement of beacons (or not) and corresponding sounds

--- New Unity Project/Assets/Scripts/Click.cs
+++ New Unity Project/Assets/Scripts/Click.cs
@@ -3,7 +3,14 @@ using System.Collections;
 
 public class Click : MonoBehaviour {
     public GameObject Beacon;
-	// Use this for initialization
+    // Use this for initialization
+    //sound clip to place object
+    public AudioClip setObject;
+    //sound clip when object can't be placed due to something in the way
+    public AudioClip noSetObjectBlock;
+    //sound clip when object can't be placed because we're broke
+    public AudioClip noSetObjectBroke;
+
 	void Start () {
 	
 	}
@@ -14,8 +21,25 @@ public class Click : MonoBehaviour {
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+            //if RayCast Hits the ground
             if (Physics.Raycast(ray, out hit))
+            {
                 Instantiate(Beacon, hit.point, transform.rotation);
+                AudioManager.Instance.PlaySound(setObject);
+            }
+            //if RayCast hits another object (can't place bc space)
+            /* else if (raycaststuff)
+            {
+                AudioManager.Instance.PlaySound(noSetObjectBlock);
+            }
+            */
+            //if RayCast hits ground, but not enough resources
+            /* else if (raycaststuff)
+            {
+                AudioManager.Instance.PlaySound(noSetObjectBroke);
+            }
+            */
+
         }
-	}
+    }
 }
