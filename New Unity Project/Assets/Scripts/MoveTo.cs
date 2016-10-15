3ecf4af27713d28047f4b2ba35cb59ccefa9fef3
nebanche <nebanche@UMICH.EDU>
2016-10-15 02:07:05 -0400

    agent additon

---
+++ New Unity Project/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTo : MonoBehaviour {
+	
+	public static Transform goal;
+	// Use this for initialization
+	void Start () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+		agent.destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Scripts/Sphere.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sphere : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		MoveTo.goal = transform; 
+		print (MoveTo.goal);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
