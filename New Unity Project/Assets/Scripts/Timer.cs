5849e6a2d6a295f137314e9dd7462087b97ceb85
hkawoosa <hkawoosa@gmail.com>
2016-10-16 16:27:10 -0400

    timer

--- New Unity Project/Assets/Scripts/Timer.cs
+++ New Unity Project/Assets/Scripts/Timer.cs
@@ -1,18 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
-    public double time;
+    public GameObject TimerGob;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        Text timerGT = TimerGob.GetComponent<Text>();
+        timerGT.text = "Time Left: 60";
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (time > 0)
+        if (Controller.S.timer > 0)
         { 
-            time -= Time.deltaTime;
+            Controller.S.timer -= Time.deltaTime;
+            Text timerGT = TimerGob.GetComponent<Text>();
+            timerGT.text = "Time Left: " + ((int)Controller.S.timer);
         }
-	}
+        else
+        {
+            //Move to new scene
+        }
+    }
 }
