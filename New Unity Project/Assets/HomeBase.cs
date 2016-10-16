7332b1feb9a81a1e6d00b56cb37b487ef186f6eb
nebanche <nebanche@UMICH.EDU>
2016-10-15 22:49:02 -0400

    some script shit

--- New Unity Project/Assets/HomeBase.cs
+++ New Unity Project/Assets/HomeBase.cs
@@ -1,17 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+//using Uni
 
 public class HomeBase : MonoBehaviour {
 
 
-
+	public Text scoreGT;
+	private int score;
 	// Use this for initialization
 	void Start () {
-	
+		score = 0;
+		GameObject scoreGob = GameObject.Find ("HighScore"); 
+		scoreGT = scoreGob.GetComponent <Text>(); 
+		scoreGT.text = "HumanCount: 0";
+
 	}
+
+
 
 	// Update is called once per frame
 	void Update () {
-	
+
+
+
 	}
+
+	// updates UI Score when the agents enter in gate 
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Agent") {
+			
+			print ("agent is home! updating score"); 
+			score += 1;
+			scoreGT.text = "HumanCount: " + score.ToString();
+			Destroy(other.gameObject); ///code here to destory agent. 
+		}
+
+
+	}
+
 }
